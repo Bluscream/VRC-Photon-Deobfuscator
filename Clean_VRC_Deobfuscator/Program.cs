f7c505adce9a671df77d9a19ce287d77f3942e5d
Colton Skees <Colton1skees@users.noreply.github.com>
2018-12-22 22:20:49 -0500

    Update Program.cs

--- Clean_VRC_Deobfuscator/Program.cs
+++ Clean_VRC_Deobfuscator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +18,11 @@ namespace Clean_VRC_Deobfuscator
             d.RenameAllEnums();
 
             Console.WriteLine("Saving Assembly... DO NOT close deobfuscator...");
-            d.RenameClassMethods();
-            d.LogAllRenamedItems();
-            
-           
-            Deobfuscator.assembly.Write(@"C:\Program Files (x86)\Steam\steamapps\common\VRChat\VRChat_Data\Managed\deobfuscatedAssembly.dll");
+                 d.RenameClassMethods();
+                d.LogAllRenamedItems(); // dass
+
+            d.CompareClasses();
+            Deobfuscator.assembly.Write(@"C:\Program Files (x86)\Steam\steamapps\common\VRChat\VRChat_Data\Managed\deobfuscatedAssemblyFinal.dll");
             Console.WriteLine("Saving finished. You may now close the deobfuscator.");
             Console.ReadLine();
         }
